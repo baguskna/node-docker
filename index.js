a325cf475d667b6b81c2541ab09aed18ed14a887
Bagus Kurnia <bagus.kurnia@sirclo.com>
2022-09-10 09:30:24 +0700

    feat: add post and user api

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const postRouter = require("./routes/postRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
+
+mongoose
+  .connect(`mongodb://database-mongo:123456@mongo-db:27017/?authSource=admin`)
+  .then(() => {
+    console.log("Database connected!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log("yuhu");
@@ -11,6 +25,9 @@ app.get("/me", (req, res) => {
   console.log("hi from me");
   res.send("<h1>okayyyy</h1>");
 });
+
+app.use("/api/v1/post", postRouter);
+app.use("/api/v1/users", userRouter);
 
 const port = process.env.PORT || 3000;
 
